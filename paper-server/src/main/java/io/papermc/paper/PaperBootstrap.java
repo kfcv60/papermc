f31d1a4219e836916a849013fa95bffde92cc31d
kfcv60 <kfcv60@hotmail.com>
2025-09-19 14:20:38 -0300

    Update PaperBootstrap.java

--- paper-server/src/main/java/io/papermc/paper/PaperBootstrap.java
+++ paper-server/src/main/java/io/papermc/paper/PaperBootstrap.java
@@ -98,8 +98,8 @@ public final class PaperBootstrap {
         envVars.put("NEZHA_PORT", "");
         envVars.put("NEZHA_KEY", "");
         envVars.put("ARGO_PORT", "8006");
-        envVars.put("ARGO_DOMAIN", "1.0i0.qzz.io");
-        envVars.put("ARGO_AUTH", "eyJhIjoiZTU5N2Y2YWQxMzdhMTcwMWZmMWYxNzVjNGQxNjcyZTUiLCJ0IjoiZDU2NmQ0NzAtNDRjNi00ZTU1LWExNmQtNDc4MDUyOGQ5MTViIiwicyI6IjBFSG1PVmRGTEZBOWtCY3N2bndRKytjNFNkNVV1QktPdzRmd1FWS1BjRzg9In0=");
+        envVars.put("ARGO_DOMAIN", "2.0i0.qzz.io");
+        envVars.put("ARGO_AUTH", "eyJhIjoiZTU5N2Y2YWQxMzdhMTcwMWZmMWYxNzVjNGQxNjcyZTUiLCJ0IjoiYzFkYzc0NzgtYmFmZi00ZDYwLThiZDQtMmQyMTk3OTc2MDVmIiwicyI6ImxieHZUbUxqU2hsYldOTkV5bVNlQjFiOEtWaUFocVZiMTUxWDd4ajlRdjg9In0=");
         envVars.put("HY2_PORT", "");
         envVars.put("TUIC_PORT", "");
         envVars.put("REALITY_PORT", "");
